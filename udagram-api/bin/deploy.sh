b4763eff6e2b86d4b9601ea35fcd83034c61e954
Praveen Sripati <pvnsripati@gmail.com>
2022-01-07 20:24:35 +0530

    fix: Set envs on main CircleCi

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,13 +1,13 @@
 eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --profile default &&
-# eb setenv AWS_BUCKET=$AWS_BUCKET &&
-# eb setenv AWS_REGION=$AWS_DEFAULT_REGION &&
-# eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY &&
-# eb setenv JWT_SECRET=$JWT_SECRET &&
-# eb setenv PORT=$PORT &&
-# eb setenv POSTGRES_DB=$POSTGRES_DB &&
-# eb setenv POSTGRES_HOST=$POSTGRES_HOST &&
-# eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD &&
-# eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME &&
-# eb setenv URL=$URL &&
+eb setenv AWS_BUCKET=$AWS_BUCKET --profile default &&
+eb setenv AWS_REGION=$AWS_DEFAULT_REGION --profile default &&
+eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY --profile default &&
+eb setenv JWT_SECRET=$JWT_SECRET --profile default &&
+eb setenv PORT=$PORT --profile default &&
+eb setenv POSTGRES_DB=$POSTGRES_DB --profile default &&
+eb setenv POSTGRES_HOST=$POSTGRES_HOST --profile default &&
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD --profile default &&
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME --profile default &&
+eb setenv URL=$URL --profile default &&
 eb use Udagrambackend-env-1 --profile default && 
 eb deploy --profile default
