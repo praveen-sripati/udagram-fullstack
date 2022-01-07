872143ea6b93e095acf59d4876bce69da7263636
Praveen Sripati <pvnsripati@gmail.com>
2022-01-07 18:44:32 +0530

    fix: Check without eb setenv

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,13 +1,13 @@
-eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID &&
-eb setenv AWS_BUCKET=$AWS_BUCKET &&
-eb setenv AWS_REGION=$AWS_DEFAULT_REGION &&
-eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY &&
-eb setenv JWT_SECRET=$JWT_SECRET &&
-eb setenv PORT=$PORT &&
-eb setenv POSTGRES_DB=$POSTGRES_DB &&
-eb setenv POSTGRES_HOST=$POSTGRES_HOST &&
-eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD &&
-eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME &&
-eb setenv URL=$URL &&
+# eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID &&
+# eb setenv AWS_BUCKET=$AWS_BUCKET &&
+# eb setenv AWS_REGION=$AWS_DEFAULT_REGION &&
+# eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY &&
+# eb setenv JWT_SECRET=$JWT_SECRET &&
+# eb setenv PORT=$PORT &&
+# eb setenv POSTGRES_DB=$POSTGRES_DB &&
+# eb setenv POSTGRES_HOST=$POSTGRES_HOST &&
+# eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD &&
+# eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME &&
+# eb setenv URL=$URL &&
 eb use Udagrambackend-env-1 --profile default && 
 eb deploy --profile default
